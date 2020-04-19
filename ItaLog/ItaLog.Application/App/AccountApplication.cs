2ebe2aa77a094186739f5abe124f571760df66c6
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-04-19 14:12:43 -0300

    Added some Identity configurations

--- ItaLog/ItaLog.Application/App/AccountApplication.cs
+++ ItaLog/ItaLog.Application/App/AccountApplication.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ItaLog.Data.Repositories
+namespace ItaLog.Application.App
 {
-    public class AuthRepository
+    public class AccountApplication
     {
     }
 }
---
+++ ItaLog/ItaLog/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItaLog.Api.Controllers
+{
+    [Route("api/[Controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {        
+    }
+}
