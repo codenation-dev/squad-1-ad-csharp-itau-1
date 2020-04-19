5961d1f337f005aa1d8f1d6d67c9759a403416ee
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-04-18 22:32:36 -0300

    Addec application tier

---
+++ ItaLog/ItaLog.Application/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItaLog.Application.AutoMapper
+{
+    public class AutoMapperConfig
+    {
+    }
+}
---
+++ ItaLog/ItaLog/Configuration/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ItaLog.Api.Configuration
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public int Expiration { get; set; }
+        public string Issuer { get; set; }
+        public string ValidAt { get; set; }
+    }
+}
