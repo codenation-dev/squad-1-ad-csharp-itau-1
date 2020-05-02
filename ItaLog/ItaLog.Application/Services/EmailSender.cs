b532712c37916b0d7055bda9331395632fd04384
Bruno Ritter <brunosk8123@hotmail.com>
2020-05-02 20:46:43 -0300

    Configuração de envio de e-mail - closed #24

--- ItaLog/ItaLog.Application/Services/EmailSender.cs
+++ ItaLog/ItaLog.Application/Services/EmailSender.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
 using MimeKit;
 using System.Threading.Tasks;
@@ -14,14 +15,23 @@ namespace ItaLog.Application.Services
         }
         public async Task SendEmailAsync(string email, string subject, string message)
         {
-            var msg = CreateEmailMessage(email, subject, message);
-            await SendEmail(msg);
+            MimeMessage mineMessage = CreateEmailMessage(email, subject, message);
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect(_emailSettings.SmtpServer, _emailSettings.Port);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(_emailSettings.UserName, _emailSettings.Password);
+
+                await client.SendAsync(mineMessage);
+                client.Disconnect(true);
+            }
         }
 
         private MimeMessage CreateEmailMessage(string email, string subject, string message)
         {
             var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress(_emailSettings.From));
+            emailMessage.From.Add(new MailboxAddress("ItaLog", _emailSettings.From));
             emailMessage.To.Add(new MailboxAddress(email));
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
@@ -30,31 +40,6 @@ namespace ItaLog.Application.Services
             };
 
             return emailMessage;
-        }
-
-        private async Task SendEmail(MimeMessage message)
-        {
-            using (var client = new MailKit.Net.Smtp.SmtpClient())
-            {
-                try
-                {
-                    client.Connect(_emailSettings.SmtpServer, _emailSettings.Port, false);
-                    client.AuthenticationMechanisms.Remove("XOAUTH2");
-                    client.Authenticate(_emailSettings.UserName, _emailSettings.Password);
-
-                    await client.SendAsync(message);
-                }
-                catch
-                {
-                    throw;
-                }
-                finally
-                {
-                    client.Disconnect(true);
-                    client.Dispose();
-                }
-            }
-
         }
     }
 }
--- ItaLog/ItaLog.Application/Services/EmailSettings.cs
+++ ItaLog/ItaLog.Application/Services/EmailSettings.cs
@@ -2,7 +2,6 @@
 {
     public class EmailSettings
     {
-        public string From { get; set; }
         public string SmtpServer { get; set; }
         public int Port { get; set; }
         public string UserName { get; set; }
