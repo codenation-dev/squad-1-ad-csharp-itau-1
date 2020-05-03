6259f6f6d962a683797122d1b04a675202af2f2d
Bruno Ritter <brunosk8123@hotmail.com>
2020-05-03 08:50:40 -0300

    Ajuste no envio de e-mail

--- ItaLog/ItaLog.Application/Services/EmailSettings.cs
+++ ItaLog/ItaLog.Application/Services/EmailSettings.cs
@@ -2,6 +2,7 @@
 {
     public class EmailSettings
     {
+        public string From { get; set; }
         public string SmtpServer { get; set; }
         public int Port { get; set; }
         public string UserName { get; set; }
